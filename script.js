d69986d8de4ae4a17d3b3d457f248a43f755d0f5
therabb1t <anacabh@gmail.com>
2023-09-01 09:23:25 -0300

    filtro

---
+++ script.js
@@ -0,0 +1,15 @@
+function aplicaFiltroCards() {
+    var input, filter, cards, cardContainer, h5, cardtitle, small, i;
+    input = document.getElementById("filtroCards");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("containerCards");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
